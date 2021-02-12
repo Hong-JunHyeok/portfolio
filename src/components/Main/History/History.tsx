3c90f2fbb33e0537526352d8e20689c607fa6ffe
HongJunHyeok <48292190+Hong-JunHyeok@users.noreply.github.com>
2021-02-12 20:17:27 +0900

    [COMPLETE]total

--- src/components/Main/History/History.tsx
+++ src/components/Main/History/History.tsx
@@ -1,8 +1,10 @@
-import React from "react"
+import React, { useState } from "react"
 import histories from "data/histories.json"
 import "./History.scss"
 import HistoryItem from "./HistoryItem"
 import { HistoryType } from "types/HistoryType"
+import Button from "components/Common/Button"
+import scrollTop from "util/scrollTop"
 
 export default function History() {
     const mapHistorys = histories.map((element: HistoryType, index) => (
@@ -10,5 +12,11 @@ export default function History() {
             {element.history}
         </HistoryItem>
     ))
-    return <ul className="histories">{mapHistorys}</ul>
+
+    return (
+        <>
+            <ul className="histories">{mapHistorys}</ul>
+            <div className="last_history"></div>
+        </>
+    )
 }
